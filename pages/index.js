b1c7d5ad6d19afaba46f0f1b839116c0ac809a4c
JufeBrown <jufebrown@gmail.com>
2022-06-12 15:54:51 -0500

    deleted unused files and uninstalled unused node modules

--- pages/index.js
+++ pages/index.js
@@ -27,9 +27,6 @@ export default function Home({ users }) {
 
 export async function getStaticProps() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  if (!response.ok) {
-    throw new Error('Failed to fetch.');
-  }
   const data = await response.json();
 
   return {
