0d49366c931ae763af796cbb0820865d2205a75b
JufeBrown <jufebrown@gmail.com>
2022-06-12 15:37:36 -0500

    made getStaticProps function to get name data

--- pages/index.js
+++ pages/index.js
@@ -22,3 +22,15 @@ export default function Home({ users }) {
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  console.log('data:', data);
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
