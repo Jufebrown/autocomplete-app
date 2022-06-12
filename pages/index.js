b66038afdbfa55bf2b5e05f1797d457b2a1d77b8
JufeBrown <jufebrown@gmail.com>
2022-06-12 15:35:48 -0500

    added users prop to home function

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+function Home({ users }) {
   return (
     <div className={styles.container}>
       <Head>
